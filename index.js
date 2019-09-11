37043b1fcc0f17c648d4346d775d7b4c303e7b51
Alan Krajina <alankrajina88@gmail.com>
2019-09-11 13:23:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ function updateDriverWithKeyAndValue(driver, key, value){
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
-  return Object.assign({ key: value }, { key: value });
+  return Object.assign({ key: value });
  }
